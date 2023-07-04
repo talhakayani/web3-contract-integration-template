84803ac14f4c2a675f87ae2e29753621cbd04a07
talhakayani <talhakayani12@gmail.com>
2023-07-04 09:46:35 +0500

    Added TODO in Constant file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,13 @@
+// TODO: Add your constants here
+
 export const ETHEREUM_CHAIN = 'polygon';
-export const STORAGECHAIN = 'storagechain';
 export const POLYGON_API = 'polygon_mombai';
 export const TOTAL_SUPPLY_MESSAGE = 'Total Supply for --{coin-name}--';
 
 export const CHAINS = {
   80001: 'Polygon Matic',
-  8726: 'StorageChain',
 };
 
 export const CHAIN_COIN_NAME = {
-  80001: 'WSTOR',
-  8726: 'STOR',
+  80001: 'MATIC',
 };
